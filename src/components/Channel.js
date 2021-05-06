0676972b7e694f786c1dd1acd7b9e511f60ba98f
nanyelo123 <nanyeloabreu89@gmail.com>
2021-05-06 16:38:44 -0400

    create component Channel

---
+++ src/components/Channel.js
@@ -0,0 +1,6 @@
+const Channel = ({ user = null  }) =>{
+
+    const [messages, setMessages] =useState ([]);
+    return <ul></ul>
+}
+export default Channel 
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Button from './Button';
+import Channel from './Channel';
+
+export {
+
+      Button,
+      Channel 
+}
