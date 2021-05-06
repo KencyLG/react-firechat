30a2eecfccf3d6510090a2f409bb3b6f7ad73eef
nanyelo123 <nanyeloabreu89@gmail.com>
2021-05-06 16:52:53 -0400

    add query for massages from firestore

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,6 +1,36 @@
-const Channel = ({ user = null  }) =>{
+import { useEffect, useState, useRef } from 'react';
+import { db, firebase } from '../config/firebase';
+import Message from './Message';
+import '../style/style.css';
 
-    const [messages, setMessages] =useState ([]);
+
+const Channel = ({ user = null }) => {
+    const [messages, setMessages] = useState([]);
+
+    //Necesito cargar los mensajes desde la BD.
+    useEffect(() => {
+        /* 
+            Crear el query de los mensajes.
+            Cargar 100 mensajes ordenados por fecha.
+        */
+        const query = db.collection('messages')
+            .orderBy('createdAt')
+            .limit(100);
+
+        const unsubscribe = query.onSnapshot(querySnapshot => {
+            //Obtiene todos los mensajes desde la bd con su ID.
+            const data = querySnapshot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            // Actualizo los mensjaes obtenidos desde la bd. 
+            setMessages(data);
+
+        });
+        //CleanUp
+        return unsubscribe;
+
+    }, [])
     return <ul></ul>
-}
+};
 export default Channel 
